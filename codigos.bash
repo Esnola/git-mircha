14a8968caa6bc7754321b4bf50289f16831ece6d
Esnola <15328708+Esnola@users.noreply.github.com>
2023-04-14 21:00:38 +0200

    Añadimos archivo de códigos

---
+++ codigos.bash
@@ -0,0 +1,77 @@
+# ayuda en la terminal
+git comando -h
+# ayuda en el navegador
+git help comando
+
+#Ignorar archivos
+#En el archivo .gitignore incluimos todo lo que NO queramos incluir en nuestro repositorio. Lo podemos crear manualmente o con gitignore.io.
+
+# esto es un comentario
+archivo.ext
+carpeta
+/archivo_desde_raiz.ext
+# ignorar todos los archivos que terminen en .log
+*.log
+# excepto production.log
+!production.log
+# ignorar los archivos terminados en .txt dentro de la carpeta doc,
+# pero no en sus subcarpetas
+doc/*.txt
+# ignorar todos los archivos terminados en .txt dentro de la carpeta doc
+# y también en sus subcarpetas
+doc/**/*.txt
+
+#Clonar repositorios
+git clone https://github.com/usuario/repositorio.git
+
+# Ramas
+# Una rama nos permite aislar una nueva funcionalidad en nuestro código que después podremos añadir a la versión principal.
+
+# crear rama
+git branch nombre-rama
+
+# cambiar de rama
+git checkout nombre-rama
+
+# crear una rama y cambiarte a ella
+git checkout -b rama
+
+# eliminar rama
+git branch -d nombre-rama
+
+# eliminar ramas remotas
+git push origin --delete nombre-rama
+
+#eliminar rama (forzado)
+git branch -D nombre-rama
+
+# listar todas las ramas del repositorio
+git branch
+
+# lista ramas no fusionadas a la rama actual
+git branch --no-merged
+
+# lista ramas fusionadas a la rama actual
+git branch --merged
+
+# rebasar ramas
+git checkout rama-secundaria
+git rebase rama-principal
+
+
+# Fusiones
+# Une dos ramas. Para hacer una fusión necesitamos:
+
+# Situarnos en la rama que se quedará con el contenido fusionado.
+# Fusionar.
+# Cuando se fusionan ramas se pueden dar 2 resultados diferentes:
+
+# Fast-Forward: La fusión se hace automática, no hay conflictos por resolver.
+# Manual Merge: La fusión hay que hacerla manual, para resolver conflictos de duplicación de contenido.
+
+
+# nos cambiamos a la rama principal que quedará de la fusión
+git checkout rama-principal
+
+# ejecutamos el comando merge con la rama secundaria a fusionar
+git merge rama-secundaria
