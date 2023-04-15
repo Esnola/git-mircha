f05b5d63aa54ee8e55de086ed61fa0ca9fb6bf3f
Esnola <15328708+Esnola@users.noreply.github.com>
2023-04-15 18:39:43 +0200

    Seguimos actualizadno codigos bash

--- codigos.bash
+++ codigos.bash
@@ -1,3 +1,23 @@
+# Configuración inicial
+# Configurando Git por primera vez
+git --version
+git config --global user.name "Jonathan MirCha"
+git config --global user.email jonmircha@gmail.com
+git config --global user.ui true
+git config --global init.defaultBranch main
+git config --list
+# asignando visual studio code como editor de configuración de git
+git config --global core.editor "code --wait"
+git config --global -e
+# para estandarizar los saltos de línea en windows
+git config --global core.autocrlf true
+# para estandarizar los saltos de línea en linux/mac
+git config --global core.autocrlf input
+# ver todas las opciones de la configuración en la terminal
+git config -h
+# ver todas las opciones de la configuración en el navegador
+git help config
+
 # ayuda en la terminal
 git comando -h
 # ayuda en el navegador
