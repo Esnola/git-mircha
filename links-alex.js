7a779cde076bfd4b1e27a6f272e5326ac09940f1
Esnola <15328708+Esnola@users.noreply.github.com>
2023-04-15 21:09:03 +0200

    Retocado archivo js

--- links-alex.js
+++ links-alex.js
@@ -1,18 +1,12 @@
 document.addEventListener('DOMContentLoaded',() =>{
-    const anclas = document.querySelectorAll('.link--anchor')
-    console.log(anclas)
-    
+    const anclas = document.querySelectorAll('.link--anchor')    
     quitaClase = ()=>{
         anclas.forEach((ancla) => {
                 ancla.classList.remove('ancla-activa')
         })
     }
     document.addEventListener('click',(e) => {
-    quitaClase();
-        if(e.target.classList.contains('link--anchor')){
-           console.log(e.target)
-            e.target.classList.add('ancla-activa')
-        }
+        quitaClase();
+        if(e.target.classList.contains('link--anchor'))  e.target.classList.add('ancla-activa')
     })
-    
 })
