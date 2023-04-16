2bae5b2ad7b7344508200e477434649452b6300d
Esnola <15328708+Esnola@users.noreply.github.com>
2023-04-16 14:34:21 +0200

    Solucionado scroll, falta perfeccionar

--- links-alex.js
+++ links-alex.js
@@ -1,46 +1,61 @@
 document.addEventListener('DOMContentLoaded',() =>{
     const anclas_enlaces = document.querySelectorAll('.link--anchor')
     posSecciones = []
-    diferencia = 100
+    diferencia = 150
+    
+
     
     anclas_enlaces.forEach((r) => {
         objHref = r.getAttribute('href')
-        indice = String( objHref)
-        posSecciones.push(document.querySelector(objHref).offsetTop)
+        nomUbica = objHref 
+        posTop = document.querySelector(objHref).offsetTop
+        posSecciones.push({nombre: nomUbica , posicion: posTop})
     })
-    console.log(posSecciones)
+    
    
     for(var clave in posSecciones) {
-       // console.log(clave+": " +posSecciones[clave]);
+       console.log(clave +": " + posSecciones[clave].nombre);
+       console.log(clave +": " + posSecciones[clave].posicion);
         
     }
-    
-    posSecciones.forEach((res,clave) =>{
-        console.log('Clave ' + clave + ' Resultado ' + res);
+    /*
+    posSecciones.forEach((res,num) =>{
+        console.log('Numero: ' + num + ' Resultado ' + res.nombre);
     })
-    
+    */
     
     quitaClase = ()=>{
         anclas_enlaces.forEach((ancla) => {
                 ancla.classList.remove('ancla-activa')
         })
     }
+    
+    
     document.addEventListener('click',(e) => {
         quitaClase();
         if(e.target.classList.contains('link--anchor')) {
-            var enlace = e.target
+            enlace = e.target
             enlace.classList.add('ancla-activa')
-            var objetivo = enlace.getAttribute('href')
-            var posTop = document.querySelector(objetivo).offsetTop
+           /* 
+                var objetivo = enlace.getAttribute('href')
+                var posTop = document.querySelector(objetivo).offsetTop
+            */
         } 
     })
     document.addEventListener('scroll',()=>{
-        maximo = posSecciones.length -1
-       for(let i=0; i < posSecciones.length; i++){
-            sig = (i <= maximo ) ? i+1 : i
-          if(scrollY >  posSecciones[i]-diferencia  && scrollY < posSecciones[sig]){
-            console.log(posSecciones[i])
-        }
-       }
+        for(var clave in posSecciones) {
+          //  console.log(clave +": " + posSecciones[clave].nombre);
+           // console.log(clave +": " + posSecciones[clave].posicion);
+            posicionA  = posSecciones[clave].posicion
+            if(scrollY > posicionA){
+                quitaClase()
+                enlace = document.querySelector('a[href^="'+ posSecciones[clave].nombre +'"]')
+               // enlaceQuitar =document.querySelector('a[href^="'+ posSecciones[clave - 1].nombre +'"]')
+               // console.log(enlace)
+               //  enlaceQuitar
+                 enlace.classList.add('ancla-activa')
+            }
+            
+         }
     })
 })
