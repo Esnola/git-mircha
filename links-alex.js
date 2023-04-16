1c7d27aa8eecbb741b94624881f5961d54dabe2a
Esnola <15328708+Esnola@users.noreply.github.com>
2023-04-16 12:43:15 +0200

    Mas actualizaciones buscando el scroll

--- links-alex.js
+++ links-alex.js
@@ -1,12 +1,46 @@
 document.addEventListener('DOMContentLoaded',() =>{
-    const anclas = document.querySelectorAll('.link--anchor')    
+    const anclas_enlaces = document.querySelectorAll('.link--anchor')
+    posSecciones = []
+    diferencia = 100
+    
+    anclas_enlaces.forEach((r) => {
+        objHref = r.getAttribute('href')
+        indice = String( objHref)
+        posSecciones.push(document.querySelector(objHref).offsetTop)
+    })
+    console.log(posSecciones)
+   
+    for(var clave in posSecciones) {
+       // console.log(clave+": " +posSecciones[clave]);
+        
+    }
+    
+    posSecciones.forEach((res,clave) =>{
+        console.log('Clave ' + clave + ' Resultado ' + res);
+    })
+    
+    
     quitaClase = ()=>{
-        anclas.forEach((ancla) => {
+        anclas_enlaces.forEach((ancla) => {
                 ancla.classList.remove('ancla-activa')
         })
     }
     document.addEventListener('click',(e) => {
         quitaClase();
-        if(e.target.classList.contains('link--anchor'))  e.target.classList.add('ancla-activa')
+        if(e.target.classList.contains('link--anchor')) {
+            var enlace = e.target
+            enlace.classList.add('ancla-activa')
+            var objetivo = enlace.getAttribute('href')
+            var posTop = document.querySelector(objetivo).offsetTop
+        } 
+    })
+    document.addEventListener('scroll',()=>{
+        maximo = posSecciones.length -1
+       for(let i=0; i < posSecciones.length; i++){
+            sig = (i <= maximo ) ? i+1 : i
+          if(scrollY >  posSecciones[i]-diferencia  && scrollY < posSecciones[sig]){
+            console.log(posSecciones[i])
+        }
+       }
     })
 })
